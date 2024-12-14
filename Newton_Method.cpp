843b8907bedca48dbee94ee08637c7f7514caa13
jija222 <bragihin@mail.ru>
2024-12-14 22:13:50 +0500

    Добавьте файлы проекта.

---
+++ Newton_Method.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+const double exactNum = 3.2300;
+
+double func(double x) {
+	return 3 * x - (4*log(x)) - 5;
+}
+
+double derivative(double x) {
+	return 3 - (4 / x);
+}
+
+double newNum(double previousNum) {
+	return previousNum - (func(previousNum) / derivative(previousNum));
+}
+
+int main() {
+	for (double i = 4; i >= exactNum; i = newNum(i)) {
+		std::cout << i << endl;
+	}
+	return 0;
+}
+
